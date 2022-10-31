645038ba1f37c18f7fd7497766626fdff05ddc56
Karl-KLT <mo1670448@gmail.com>
2022-10-31 11:29:03 -0700

    fBugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,7 +84,7 @@
         </nav>
     </div>
     <!--Container Main start-->
-    @if (!env('DevSecure'))
+    @if (env('DevSecure'))
         <div class="badge bg-danger p-3 fw-bold mt-4" style="letter-spacing: 0.8px">
             error: working on site to be more secure
         </div>
