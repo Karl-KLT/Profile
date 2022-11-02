6c35322dc8028ee2466f65f1b3c9e9f6e9b4b690
Karl-KLT <mo1670448@gmail.com>
2022-11-01 17:17:16 -0700

    FixB

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'stripe/*',
-        'voting/*/embed',
-        'livewire/message/embed-voting', 
+        'livewire/message/auth',
     ];
 }
