fe183acfebbc8eaee40ec469680b4a636c644298
Karl-KLT <mo1670448@gmail.com>
2022-11-14 03:41:44 -0800

    add user photo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
     @yield('custom-head')
     <script src="{{ mix('js/app.js') }}"></script>
     <title>@yield('Title',env('APP_NAME'))</title>
