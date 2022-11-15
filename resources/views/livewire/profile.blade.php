f05d306f599dcc14950e687f463f7f36abf588f9
Karl-KLT <mo1670448@gmail.com>
2022-11-15 11:56:05 -0800

    fixSomeChanges

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -13,9 +13,8 @@
 
             <div>
                 <div id="select_img" class="cursor-pointer border rounded-circle">
-                    <img src="{{ url('storage/imgs/default-user-image.png') }}" alt="">
-                    {{ dd(url('storage/imgs/default-user-image.png')) }}
-                    <img wire:ignore style="width: 155px;height: 120px;border-radius: 100%;" id="img" src="{{ $Image ? asset($Image) : url('storage/imgs/default-user-image.png') }}" alt="Profile_image">
+
+                    <img wire:ignore style="width: 155px;height: 120px;border-radius: 100%;" id="img" src="{{ $Image ? asset($Image) : asset('storage/imgs/default-user-image.png') }}" alt="Profile_image">
 
                 </div>
 
