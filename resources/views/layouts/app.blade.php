bea73755b28433886beefe204845f8e8c16be343
Karl-KLT <mo1670448@gmail.com>
2022-11-23 08:44:09 -0800

    add profile photo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 <body id="body-pd">
     <header class="header" id="header">
 
-        <div class="header_toggle"
+        <div class="header_toggle">
             <i class='bx bx-menu' id="header-toggle"></i>
         </div>
         {{-- <div class="header_img">
