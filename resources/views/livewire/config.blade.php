2cd3374e1f931a1c262cee39ec5fd43e6452e893
Karl-KLT <mo1670448@gmail.com>
2022-11-25 07:09:17 -0800

    change prom for users

--- resources/views/livewire/config.blade.php
+++ resources/views/livewire/config.blade.php
@@ -19,25 +19,31 @@
                         <td class="text-center">{{ $user->Profile->Name }}</td>
                         <td class="text-center">{{ $user->email }}</td>
                         <td class="text-center" style="user-select: text;">{{ $user->Profile->USER_SID }}</td>
-                        @if($user->email == auth()->user()->email)
-                            <td class="text-center">{{ $user->Profile->userType ? 'admin' : 'user' }} ( you )</td>
+                        @if (env('MAIN_ADMIN_EMAIL') != $user->email)
+
+                            @if($user->email == auth()->user()->email)
+                                <td class="text-center">{{ $user->Profile->userType ? 'admin' : 'user' }} ( you )</td>
+                            @else
+                                <td class="text-center">
+
+                                    <form action="{{ route('saveProm') }}" method="POST">
+                                        @csrf
+
+                                        <select name="prom" id="prom" class="text-dark">
+                                            <option value="{{ $user->Profile->userType ? 1 : 0 }}" selected>{{ $user->Profile->userType ? 'admin' : 'user' }}</option>
+                                            <option value="{{ $user->Profile->userType ? 0 : 1 }}">{{ $user->Profile->userType ? 'user' : 'admin' }}</option>
+                                        </select>
+
+                                        <button id="sub" type="submit" hidden></button>
+                                        <input type="text" name="id" value="{{ $user->id }}" hidden>
+                                        <script>document.getElementById('prom').addEventListener('change',function(){document.getElementById('sub').click();})</script>
+                                    </form>
+
+                                </td>
+                            @endif
+
                         @else
-                            <td class="text-center">
-
-                                <form action="{{ route('saveProm') }}" method="POST">
-                                    @csrf
-
-                                    <select name="prom" id="prom" class="text-dark">
-                                        <option value="{{ $user->Profile->userType ? 1 : 0 }}" selected>{{ $user->Profile->userType ? 'admin' : 'user' }}</option>
-                                        <option value="{{ $user->Profile->userType ? 0 : 1 }}">{{ $user->Profile->userType ? 'user' : 'admin' }}</option>
-                                    </select>
-
-                                    <button id="sub" type="submit" hidden></button>
-                                    <input type="text" name="id" value="{{ $user->id }}" hidden>
-                                    <script>document.getElementById('prom').addEventListener('change',function(){document.getElementById('sub').click();})</script>
-                                </form>
-
-                            </td>
+                            <td class="text-center">not available</td>
                         @endif
                     </tr>
                 @endforeach
